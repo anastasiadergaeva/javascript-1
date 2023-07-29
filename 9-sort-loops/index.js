ab4b5b90829173a67668bc59ad6a141d29d5aa71
anastasiadergaeva <adergaeva@gmail.com>
2023-07-29 18:15:54 +0300

    change loop

--- 9-sort-loops/index.js
+++ 9-sort-loops/index.js
@@ -1,23 +1,10 @@
 const arr = [1, 40, -5, 19, 0];
 
-// function sortArray() {
-//     for (let i = 0; i < arr.length; i++){
-//         for (let j = 0; j < arr.length - 1; j++) {
-//             if (arr[j] > arr[j + 1]) {
-//                 const newCount = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = newCount;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-
 function sortArray() {
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 0; j < arr.length - 1; j++) {
+    for (let i = 0; i < arr.length - 1; i++){
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
     }
