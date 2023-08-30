2ec7ecb7ebc34bc96f6974565064abfa33da149f
anastasiadergaeva <adergaeva@gmail.com>
2023-08-30 14:28:42 +0300

    update 11-date-array

--- 11-date-array/index.js
+++ 11-date-array/index.js
@@ -1,18 +1,17 @@
-const incomingStrings = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
-const filteredDates = [];
+const incomingStrings = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023', '31-04-2000', '30-02-2002'];
 
-function filterDates (arrayOfDates) {
-    const arrayWithoutText = arrayOfDates
-    .map((stringValue) => stringValue.split(/[-/]/))
-    .filter(stringValue => {
-        return Number(stringValue[1]);
-    });
-    for (i = 0; i < arrayWithoutText.length; i++) {
-        if (arrayWithoutText[i][0] > 0 && arrayWithoutText[i][0] < 31) {
-            filteredDates.push(arrayWithoutText[i].join('-'));
-        }
+function filterDates(arrayOfDates) {
+    const filteredArr = arrayOfDates.flatMap((stringValue) => {
+    const date = new Date(stringValue);
+    if (!isNaN(date.getTime())) {
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+        return [`${day}-${month}-${year}`];
     }
-    return filteredDates;
+    return [];
+    });
+    return filteredArr;
 }
 
 console.log(filterDates(incomingStrings));
